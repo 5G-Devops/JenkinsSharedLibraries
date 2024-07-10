28df8f662cc87cc5efc21674e552a5bbe62b016a
5G-Devops <dvpsdmndmn@gmail.com>
2024-07-10 17:25:47 +0530

    commit

--- vars/imageScan.groovy
+++ vars/imageScan.groovy
@@ -1,6 +1,6 @@
 def call(String repositoryname, String imagename, String tagname ){
     sh """
-      trivy image ${repositoryname}/${imagename} ${repositoryname}/${imagename}:latest > scan.txt
+      trivy image ${repositoryname}/${imagename}:latest > scan.txt
       cat scan.txt
     """
 }
