636b2ad82ceab2b2833ea37513db7cae09af5708
5G-Devops <dvpsdmndmn@gmail.com>
2024-07-10 18:04:56 +0530

    commit

--- vars/imagePush.groovy
+++ vars/imagePush.groovy
@@ -3,7 +3,7 @@ def call(String repositoryname, String imagename, String tagname){
     sh "docker login -u '$USER' -p '$PASSWORD'"
 }
 sh """
-docker push ${repositoryname}/${imagename}  ${repositoryname}/${imagename}:${tagname}
-docker push ${repositoryname}/${imagename}  ${repositoryname}/${imagename}:latest
+docker push  ${repositoryname}/${imagename}:${tagname}
+docker push  ${repositoryname}/${imagename}:latest
 """
 }
