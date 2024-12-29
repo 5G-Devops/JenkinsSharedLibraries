3d446a95827ef428f26574bbcf254327e3b8c06b
5G-Devops <dvpsdmndmn@gmail.com>
2024-12-29 22:04:52 +0530

    update

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,3 +1,12 @@
-def call(){
+def call(Map stageParams){
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: stageParams.branch]],
+        userRemoteConfigs: [[url: stageParams.url]]
+        
+        
+          ])
+
+    
     
 }
