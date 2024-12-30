e4092848815a16d2cfefbab81ce04e891b1468fd
5G-Devops <dvpsdmndmn@gmail.com>
2024-12-30 15:40:52 +0530

    update

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,6 +2,6 @@ def call(String repositoryname, String imagename, String tagname ){
     sh """
       docker build -t ${repositoryname}/${imagename} .
       docker image tag ${repositoryname}/${imagename}  ${repositoryname}/${imagename}:${tagname}
-      docker image tag ${repositoryname}/${imagename} ${repositoryname}/${imagename}:latest
+      docker image tag ${repositoryname}/${imagename}  ${repositoryname}/${imagename}:latest
     """
 }
--- vars/imageScan.groovy
+++ vars/imageScan.groovy
@@ -1,6 +1,6 @@
 def call(String repositoryname, String imagename, String tagname ){
     sh """
-      trivy image ${repositoryname}/${imagename}:latest > scan.txt
-      cat scan.txt
+      trivy image --format table -o imagescan.html ${repositoryname}/${imagename}:latest
+      cat imagescan.html
     """
 }
