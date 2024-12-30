b8e53833137a99a7df1f41c45912e3f5abb655ba
5G-Devops <dvpsdmndmn@gmail.com>
2024-12-30 16:23:05 +0530

    update

--- vars/imagePush.groovy
+++ vars/imagePush.groovy
@@ -1,7 +1,13 @@
 def call(String repositoryname, String imagename, String tagname){
   /*  withCredentials([usernamePassword(credentialsId: 'docker-token', passwordVariable: 'PASSWORD', usernameVariable: 'USER')]) {
     sh "docker login -u '$USER' -p '$PASSWORD'"
-} */
+} 
+sh """
+     docker push  ${repositoryname}/${imagename}:${tagname}
+     docker push  ${repositoryname}/${imagename}:latest
+     """
+     */
+     // OR
 withDockerRegistry(credentialsId: 'docker-token', toolName: 'docker') {
     sh """
      docker push  ${repositoryname}/${imagename}:${tagname}
